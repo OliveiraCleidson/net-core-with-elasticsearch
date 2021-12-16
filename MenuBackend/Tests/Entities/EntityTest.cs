dd3d5fce5762f6b66c7b778e31818ddcbc175869
Cleidson Oliveira <dev.cleidson@gmail.com>
2021-12-16 19:20:01 -0300

    test: ensure entity

--- MenuBackend/Tests/Entities/EntityTest.cs
+++ MenuBackend/Tests/Entities/EntityTest.cs
@@ -1,6 +1,26 @@
+using System;
+using Domain.Entities;
+using Xunit;
+
 namespace Tests.Entities;
 
 public class EntityTest
 {
-    
+
+    [Fact]
+    public void Entity_Should_Generate_Id()
+    {
+        var sut = new Entity();
+
+        Assert.NotEqual(Guid.Empty, sut.Id);
+    }
+
+    [Fact]
+    public void Entity_Should_Initialize_CreatedAt_And_UpdatedAt()
+    {
+        var sut = new Entity();
+        
+        Assert.NotEqual(DateTime.MinValue,sut.CreatedAt);
+        Assert.NotEqual(DateTime.MinValue,sut.UpdateAt);
+    }
 }
